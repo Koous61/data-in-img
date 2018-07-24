0b582a767c99aaece786e86b0180d6c0626c8bab
Koous <dima_nichiporchik@mail.ru>
2018-07-24 18:28:45 +0300

    Test bin and image

--- src/io/ImageIOImpl.java
+++ src/io/ImageIOImpl.java
@@ -1,21 +1,20 @@
 package io;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class ImageIOImpl<I> implements ImageIO<I> {
     @Override
-    public I load(String path) {
-        try {
+    public  I load(String path) throws IOException {
             return (I) javax.imageio.ImageIO.read(new File(path));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     @Override
-    public Boolean save(String path) {
+    public Boolean save(I data, String path) throws IOException {
+        File outputFile = new File(path);
+        outputFile.getParentFile().mkdirs();
+        javax.imageio.ImageIO.write((BufferedImage) data, "jpg", outputFile);
         return null;
     }
 }
